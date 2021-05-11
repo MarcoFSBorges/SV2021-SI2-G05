b0e73421da0638f4c670e1dadff728f48f32b40b
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-05-11 18:04:09 +0100

    Inserts are done, some refactor on createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -17,19 +17,20 @@ BEGIN TRY
     BEGIN TRANSACTION
         CREATE TABLE PLAYER(
             player_id int IDENTITY(0,1),  --starts at 0 and increments one for every entry
-            name VARCHAR(30) NOT NULL,
+            username varchar(50) unique NOT NULL,
             deleted BIT default 0,
             PRIMARY KEY (player_id)
         );
 
         CREATE TABLE LOGIN(
             login_id int IDENTITY(0,1),
-            user_email varchar unique NOT NULL,
-            username varchar(50) unique NOT NULL,
-            name varchar(50) NOT NULL,
+            user_email varchar(50) unique NOT NULL,
+            username varchar(50),
+            name VARCHAR(30) NOT NULL,
             password varchar(100) NOT NULL,
             birthday DATE NOT NULL,
             PRIMARY KEY (login_id),
+            FOREIGN KEY (username) REFERENCES PLAYER(username),
             CONSTRAINT chk_email CHECK (user_email LIKE ('%_@__%.__%')),
         );
 
@@ -37,27 +38,27 @@ BEGIN TRY
             config_id int IDENTITY(0,1),
             max_level int not null,
             clan_members int not null,
-            limit int not null,
+            limit int not null, --limiar
             CONSTRAINT chk_min_config CHECK (max_level > 0 AND clan_members > 0 AND limit > 0)
         );
 
          CREATE TABLE CLAN(
             clan_id int IDENTITY(0,1),
             clan_name VARCHAR(20) NOT NULL,
-            clan_score INTEGER,
+            clan_score int default 0,
             PRIMARY KEY (clan_id),
         );
 
         CREATE TABLE REGISTEREDPLAYER(
-            player_id int IDENTITY(0,1),
-            login_id INTEGER,
-            score INTEGER NOT NULL,
-            level INTEGER NOT NULL,
-            bank_balance INTEGER NOT NULL,
+            player_id int,
+            login_id int,
+            score INTEGER default 0,
+            level INTEGER default 1,
+            bank_balance INTEGER default 0,
             life_points INTEGER NOT NULL,
             strength_points INTEGER NOT NULL,
             speed_points INTEGER NOT NULL,
-            clan int NOT NULL,
+            clan int default -1,
             PRIMARY KEY (player_id),
             FOREIGN KEY (player_id) REFERENCES PLAYER(player_id),
             FOREIGN KEY (clan) REFERENCES CLAN(clan_id),
@@ -67,22 +68,23 @@ BEGIN TRY
         );
 
         CREATE TABLE NONREGISTEREDPLAYER(
-            player_id INTEGER,
-            name VARCHAR(30) NOT NULL,
+            player_id int,
+            name VARCHAR(30) default CONVERT(varchar(50), NEWID()),
             PRIMARY KEY (player_id),
             FOREIGN KEY (player_id) REFERENCES PLAYER(player_id)
         );
 
         CREATE TABLE STATE(
             state_id integer primary key,
-            state varchar not null
+            state varchar(20) not null
         );
 
         CREATE TABLE TYPE(
-            type_id int IDENTITY(0,1),
-            min_life_points INTEGER NOT NULL,
-            min_strength_points INTEGER NOT NULL,
-            min_speed_points INTEGER NOT NULL,
+            type_id int primary key,
+            description varchar(50) not null,
+            min_life_points INTEGER default 1,
+            min_strength_points INTEGER default 1,
+            min_speed_points INTEGER default 1,
             CONSTRAINT chk_min_type CHECK (min_life_points >= 0 AND min_strength_points >= 0 AND min_speed_points >= 0)
         );
 
@@ -90,34 +92,33 @@ BEGIN TRY
             match_id int IDENTITY(0,1),
             state_id integer,
             type_id int,
-            player_one INTEGER,
+            player_one INTEGER not null,
             player_two INTEGER,
             start_time DATE default CURRENT_TIMESTAMP,
             winner INTEGER,
             PRIMARY KEY (match_id),
+            FOREIGN KEY (state_id) REFERENCES STATE(state_id),
+            FOREIGN KEY (type_id) REFERENCES TYPE(type_id),
             FOREIGN KEY (player_one) REFERENCES PLAYER(player_id),
             FOREIGN KEY (player_two) REFERENCES PLAYER(player_id),
             FOREIGN KEY (winner) REFERENCES PLAYER(player_id)
         );
 
-
-
         CREATE TABLE ITEM(
               item_id int IDENTITY(0,1),
               player_id int default null,  --pode existir item sem estar referênciado a um player, para dar opções por exemplo em menus em caixas, etc
               name varchar(100) NOT NULL,
-              bonus_life decimal(2, 2) default 0,
-              bonus_strength decimal(2, 2) default 0,
-              bonus_speed decimal(2, 2) default 0,
+              bonus_life decimal(4, 2) default 0,
+              bonus_strength decimal(4, 2) default 0,
+              bonus_speed decimal(4, 2) default 0,
               PRIMARY KEY(item_id),
               CONSTRAINT check_minimum CHECK (bonus_life >= 0 AND bonus_strength >= 0 AND bonus_speed >= 0),
               FOREIGN KEY (player_id) REFERENCES PLAYER(player_id)
         );
 
         CREATE TABLE ITEM_PRICE(
-            item_id integer check (item_id >= 0),
+            item_id int,
             price decimal(20, 3) check (price >= 0),
-            --discount decimal(18, 3) not null default 0 check (discount >= 0 and discount <= 1),  --enunciado não referem discutir em grupo
             FOREIGN KEY (item_id) REFERENCES ITEM(item_id)
         );
 
---
+++ insert_tables.sql
@@ -0,0 +1,78 @@
+set transaction isolation level serializable
+BEGIN TRY
+	BEGIN TRANSACTION
+    INSERT INTO PLAYER (username) VALUES
+        ('MalhaVacas'),
+        ('MestreDelas'),
+        ('TheTherminator'),
+        ('Muchacha'),
+        ('AnitaPau'),
+        ('VivaAoPipes'),
+        ('MigustaTu'),
+        ('GetReal');
+
+    INSERT INTO LOGIN (user_email, username, name, password, birthday) VALUES
+        ('MarcoBorges@gmail.com', 'MalhaVacas', 'Marco Borges', 'mgnevpa70h822Kx', '1996-05-03'),
+        ('PedroPires@gmail.com', 'MestreDelas', 'Pedro Pires', 'rAXz5bR3DdqnVyI', '1999-10-08'),
+        ('DanielAzevedo@hotmail.com', 'TheTherminator', 'Daniel Azevedo', 'VNl8iccweptzX2r', '1994-01-06'),
+        ('LeiaCambezes@gmail.com', 'Muchacha', 'Leia Cambezes', 'lPl5z43VaB4JcTc', '1976-07-23'),
+        ('AnitaAlpuim@hotmail.com', 'AnitaPau', 'Anita Alpuim', 'HORwhUKMtaZGyVX', '1983-05-24'),
+        ('BogdanPaiva@gmail.com', 'VivaAoPipes', 'Bogdan Paiva', 'RGoHHicOSFzNi85', '2002-11-21'),
+        ('AtílioEstrada@gmail.com', 'MigustaTu', 'Atílio Estrada', 'xS555GIcspRp08g', '1999-12-20'),
+        ('LuenaNegrão@hotmail.com', 'GetReal', 'Luena Negrão', 'PAosMVGLFa6DLCF', '1984-06-18');
+
+    INSERT INTO GLOBAL_CONFIGURATION (max_level, clan_members, limit) VALUES
+        (50, 14, 20),
+        (45, 12, 53),
+        (54, 13, 52),
+        (80, 13, 52);
+
+    INSERT INTO CLAN (clan_name) VALUES
+        ('Onyx Pygmy Squad'),
+        ('Ivory Dragontooth'),
+        ('Gold Knife Crew'),
+        ('Moth Posse'),
+        ('Flame Pincer Gang'),
+        ('Cobalt Enigma Clan'),
+        ('Gold Elephant Riders');
+
+    INSERT INTO STATE (state_id, state) VALUES
+        (1, 'Por iniciar'),
+        (2, 'A decorrer'),
+        (3, 'Terminada'),
+        (4, 'Contabilizada');
+
+    INSERT INTO TYPE (type_id, description, min_life_points, min_strength_points, min_speed_points) VALUES
+        (1, 'DARKNIGHT MODE', 100, 50, 30),
+        (2, 'FUCKING IMPOSSIBLE', 70, 36, 15),
+        (3, 'KIM POSSIBLE', 10, 5, 10),
+        (4, 'GOD OF WAR', 999, 999, 999);
+
+    INSERT INTO MATCH (state_id, type_id, player_one) VALUES
+        (1, 1, 1),
+        (3, 2, 0),
+        (4, 3, 2),
+        (2, 3, 5),
+        (2, 4, 3);
+
+    INSERT INTO ITEM (player_id, name , bonus_life, bonus_strength, bonus_speed) VALUES
+        (1, 'Diamond Sword', 1.50, 8.20, 2.00),
+        (3, 'Magic Umbrella', 6.00, 2.40, 1.10),
+        (4, 'Radiant Armor', 1.21, 8.00, 0.00),
+        (2, 'Soccer Ball',10.00 ,0.00 ,60.00),
+        (1, 'Invisibility Cap',0 ,0 ,8.40),
+        (3, 'The sword of kings',5.50 ,5.50 ,0);
+
+    INSERT INTO ITEM_PRICE (item_id, price) VALUES
+        (6, 100000),
+        (7, 500.69),
+        (8, 9900000),
+        (9, 9999999),
+        (10, 54000),
+        (11, 100);
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	ROLLBACK
+	raiserror('Failed tables filling',16,1)
+END CATCH
