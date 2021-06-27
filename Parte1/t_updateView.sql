03cc3d0ca78e5ce7991c1956fb7f1bd82efba3ac
Marco <marcofsborges@gmail.com>
2021-06-27 12:32:49 +0100

    repo cleanup

--- Parte1/t_updateView.sql
+++
@@ -1,9 +0,0 @@
-create or alter trigger update_players_view on players_view
-instead of update
-as begin
-    declare @item_id int
-    declare @player_id int
-    select @item_id = item_id from inserted
-    select @player_id = player_id from inserted
-    update ITEM set active = 1, player_id = @player_id where item_id = @item_id
-end
